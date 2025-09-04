8b347a621bcdd0b2ce2e181b6c93bd06b78fe294
Norbiazseni <siposnorbert2007@gmail.com>
2025-09-04 10:47:48 +0200

    variables.php

---
+++ variables.php
@@ -0,0 +1,18 @@
+<?php
+
+    /*
+        szükséges programok: XAMPP (Apache, MySQL), Git, github repo, VSC
+        <?php ..... ?>
+
+        Változók:
+    */
+
+        $name = "Norbi";
+        $age = 18;
+        $city = "Karcag";
+        var_dump($name);
+
+        echo "\n<br>Név: {$name} Életkor: {$age} Lakóhely: {$city}"
+
+
+?>
