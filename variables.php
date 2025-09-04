4a9058151908b3f63344fa824d354a393905f6b4
Norbiazseni <siposnorbert2007@gmail.com>
2025-09-04 11:20:50 +0200

    variables.php

--- variables.php
+++ variables.php
@@ -12,7 +12,39 @@
         $city = "Karcag";
         var_dump($name);
 
-        echo "\n<br>Név: {$name} Életkor: {$age} Lakóhely: {$city}"
+        echo "\n<br>Név: {$name} Életkor: {$age} Lakóhely: {$city}";
+
+
+        //Konstans
+        define("PI", 3.14);
+        echo "<br>";
+
+        //adattípusok: string, int, float, bool, array
+
+        $message = "egy";
+
+        echo "Kiír", $message, "értéket.", "<br>\n.";
+        echo "Kiír $message értéket. <br>\n";
+        echo "Kiír {$message}darab értéket. <br>\n";
+        echo 'Kiír $message értéket. <br>\n';
+
+        print "Kiír". $message. "értéket."."<br>\n";
+        print "Kiír $message. értéket." "<br>\n";
+        print 'Kiír. {$message}. értéket. <br>\n';
+
+        /*
+
+        git parancsok
+            git init: helyi repó inicializálása
+            git add .: Módosított fájlok hozzáadása a staging area-hoz.
+            git commit -m "message": Változtatott fájlok commitálása
+            git remote add origin https://github.como/felnev/reponev.git
+                git config --global user.name illetve e-mail: 
+            git branch -M main: "A fő ág(main) nevezése a branchben(mivan???)"
+            git push -u origin main: feltölti
+
+            HF: töltsd le otthon a repot a saját htdocs mappádba
+        */
 
 
 ?>
