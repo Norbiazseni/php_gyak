79d79bc6e99ccbaa5fe5cc185873c9741368ba23
Norbiazseni <siposnorbert2007@gmail.com>
2025-09-15 08:05:40 +0200

    get_post_init

---
+++ Form/get_post.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Űrlapkezelés</h1>
+    <form action="" method="post">
+        <label for="name">Név:</label>
+        <input type="text" id="name">
+        <button type="submit">Küld</button>
+    </form>
+</body>
+</html>
